87bfa93e502a3235d123521670d68f4c4d661af8
mateus-agostinho <mateusagostinho123@gmail.com>
2022-11-06 17:00:06 -0500

    primeiro commit

---
+++ sanduiches.php
@@ -0,0 +1,34 @@
+<?php
+    class Sanduiches{
+        private $id;
+        private $nome;
+        private $valor;
+        private $descricao;
+
+        function setId($id){
+            $this->id = $id;
+        }
+        function getId(){
+            return $this->id;
+        }
+        function setNome($nome){
+            $this->nome = $nome;
+        }
+        function getNome(){
+            return $this->nome;
+        }
+        function setValor($valor){
+            $this->valor = $valor;
+        }
+        function getValor(){
+            return $this->valor;
+        }
+        function setDescricao($descricao){
+            $this->descricao = $descricao;
+        }
+        function getDescricao(){
+            return $this->descricao;
+        }
+        
+    }
+?>
